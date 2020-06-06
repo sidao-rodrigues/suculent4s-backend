ab85305324c1cad103aa41c2f477b682d8fae5b5
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-06-06 18:02:43 -0300

    criando projeto backend e configurando dependencias do spring boot starter.

---
+++ src/main/java/br/com/suculent4s/MarketplaceApplication.java
@@ -0,0 +1,12 @@
+package br.com.suculent4s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceApplication.class, args);
+    }
+}
